20487a570f9b46957b89c0adbc08629e19722335
ohisee <breakosin@hotmail.com>
2022-04-06 01:07:36 -0700

    animation and style

--- reviewstyling/src/app/components/course-project/course-project.component.ts
+++ reviewstyling/src/app/components/course-project/course-project.component.ts
@@ -1,9 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { animate, animateChild, group, query, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-course-project',
   templateUrl: './course-project.component.html',
-  styleUrls: ['./course-project.component.css']
+  styleUrls: ['./course-project.component.css'],
+  animations: [
+    trigger('routeState', [
+      // rootPage => userPage
+      transition('* => *', [
+        group([
+          query(':enter', [
+            animateChild(),
+            style({
+              transform: 'translateY(-400px)',
+              opacity: 0
+            }),
+            animate('300ms ease-out'),
+          ], { optional: true }),
+          query(':leave', [
+            animate('300ms ease-out', style({
+              transform: 'translateY(600px)',
+              opacity: 0
+            }))
+          ], { optional: true })
+        ])
+      ])
+    ])
+  ]
 })
 export class CourseProjectComponent implements OnInit {
 
@@ -12,4 +37,18 @@ export class CourseProjectComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /**
+   * Get route data from routing module routes
+   * path: 'course-project', component: CourseProjectComponent, children: [
+      { path: '', component: ProjectsComponent, data: {animation: {page: 'rootPage'}} },
+      { path: 'users', component: UsersComponent, data: {animation: {page: 'userPage'}} },
+    ]
+   */
+  getAnimationData(outlet: RouterOutlet): string {
+    const routeData = outlet.activatedRouteData['animation'];
+    if (!routeData) {
+      return 'rootPage';
+    }
+    return routeData['page'];
+  }
 }
