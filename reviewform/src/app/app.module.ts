98c18de53c2f6114417b8e404e4e1c50c5e1212b
ohisee <breakosin@hotmail.com>
2022-05-04 14:31:21 -0700

    template and reactive forms

---
+++ reviewform/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './components/template-form/template-form.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormComponent,
+    ReactiveFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule, // build forms using reactive creating form
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
