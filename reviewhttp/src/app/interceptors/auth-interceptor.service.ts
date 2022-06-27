91d32cdc470ac4bd77e92da0af72e04c616bdcd2
ohisee <breakosin@hotmail.com>
2022-06-27 00:04:32 -0700

    how to use http client

---
+++ reviewhttp/src/app/interceptors/auth-interceptor.service.ts
@@ -0,0 +1,15 @@
+/**
+ * @description interceptor
+ */
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('Request is on its way', req.url);
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('Auth', 'xyz')
+    });
+    return next.handle(modifiedRequest);
+  }
+}
