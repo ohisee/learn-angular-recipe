a690d1b9e358f1b2bc22680df397ff482502d651
ohisee <breakosin@hotmail.com>
2022-07-20 00:08:21 -0700

    build a recipe

---
+++ reviewrecipe/src/app/app-routing.module.ts
@@ -0,0 +1,45 @@
+/**
+ * @fileoverview routing module
+ */
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  // {
+  //   path: 'recipes',
+  //   component: RecipesComponent,
+  //   canActivate: [AuthGuard],
+  //   children:
+  //     [
+  //       { path: '', component: RecipeStartComponent },
+  //       { path: 'new', component: RecipeEditComponent }, // must be before :id route to avoid parsing 'new' as id
+  //       { path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService] },
+  //       { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] }
+  //     ]
+  // },
+  // { path: 'shopping-list', component: ShoppingListComponent },
+  // { path: 'auth', component: AuthComponent }
+
+  // lazy loading, must remove from importing RecipesModule in app module ts file 
+  {
+    path: 'recipes',
+    loadChildren: () => import('./components/recipes/recipes.module').then(m => m.RecipesModule)
+  },
+  // lazy loading, must remove from importing ShoppingListModule in app module ts file 
+  {
+    path: 'shopping-list',
+    loadChildren: () => import('./components/shopping-list/shopping-list.module').then(m => m.ShoppingListModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true, preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
