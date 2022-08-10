85490f256a2f6d9cc33a50ebfa39083516822ee7
ohisee <breakosin@hotmail.com>
2022-08-10 01:03:56 -0700

    build a recipe

---
+++ reviewrecipe/src/app/components/recipes/recipes.module.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipesComponent } from "./recipes.component";
+import { RecipesRoutingModule } from "./recipes-routing.module";
+import { SharedModule } from "src/app/shared/shared.module";
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ],
+  imports: [
+    CommonModule, // use CommonModule instead of BrowserModule
+    ReactiveFormsModule,
+    RecipesRoutingModule,
+    RouterModule,
+    SharedModule,
+  ],
+})
+export class RecipesModule { }
+
