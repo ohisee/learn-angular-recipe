d4657ff80a3483d2489bbd27ecb6d60e25a90d5c
ohisee <breakosin@hotmail.com>
2022-08-27 00:16:22 -0700

    build a recipe

---
+++ reviewrecipe/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,60 @@
+/**
+ * @fileoverview recipe list component
+ */
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { ReplaySubject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit, OnDestroy {
+  recipes: Recipe[] = [];
+
+  private replaySubject$ = new ReplaySubject<void>();
+
+  /**
+   * use router outlet instead of emitting event
+   * @deprecated
+   */
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
+
+  constructor(
+    private readonly recipeService: RecipeService,
+    private readonly router: Router,
+    private readonly activatedRoute: ActivatedRoute) {}
+
+  /**
+   * use router outlet instead of emitting event 
+   * @deprecated
+   */
+  public onSelect(recipe: Recipe): void {
+    this.selectedRecipe.emit(recipe);
+  }
+
+  public ngOnInit(): void {
+    this.recipeService.recipesChanged$.pipe(
+      takeUntil(this.replaySubject$)
+    ).subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes;
+      }
+    );
+    this.recipes = this.recipeService.getRecipes();
+  }
+
+  public onClickToNewRecipe(): void {
+    this.router.navigate(['new'], { relativeTo: this.activatedRoute });
+  }
+
+  ngOnDestroy(): void {
+    this.replaySubject$.next();
+    this.replaySubject$.complete();
+  }
+}
+
