e35b3e8348fcbca2bfde5b33d8228929aa281a44
ohisee <breakosin@hotmail.com>
2022-11-03 00:19:52 -0700

    standalone components

---
+++ standalonecomponents/src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { DetailsComponent } from './details/details.component';
+
+@Component({
+  standalone: true,
+  imports: [DetailsComponent],
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
+
