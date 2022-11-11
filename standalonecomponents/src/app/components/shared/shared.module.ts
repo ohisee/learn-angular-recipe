a0adc1ff429116aad2e711ce65bc26c4886d9604
ohisee <breakosin@hotmail.com>
2022-11-11 00:34:44 -0800

    standalone components

---
+++ standalonecomponents/src/app/components/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+
+// import { HighlightDirective } from "./highlight.directive";
+
+/** This ng module file is no needed anymore after switching to standalong component. */
+@NgModule({
+  declarations: [
+    // HighlightDirective, // no need in declarations after making it standalong
+  ],
+  exports: [
+    // HighlightDirective, // no need in exports after making it standalong
+  ]
+})
+export class SharedModule { }
+
