a46413f6aa81cee0a1a931a4e59e38ca8b6818a4
ohisee <breakosin@hotmail.com>
2022-12-07 00:40:27 -0800

    build a recipe with ngrx

---
+++ reviewrecipengrx/src/app/components/auth/model/auth-response-data.ts
@@ -0,0 +1,12 @@
+/**
+ * auth response data interface
+ */
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
