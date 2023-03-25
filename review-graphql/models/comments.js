ee7d3d5ce3b83233196f735a967c71961a5dd64f
ohisee <breakosin@hotmail.com>
2023-03-25 00:36:30 -0700

    review graphql

---
+++ review-graphql/models/comments.js
@@ -0,0 +1,23 @@
+
+/**
+ * @typedef Comment
+ * @type {object}
+ * @property {string} id
+ * @property {string} text
+ */
+
+/** @type Comment[] */
+const Comments = [
+  {
+    id: '1',
+    text: 'comment number 1'
+  }, {
+    id: '2',
+    text: 'comment number 2'
+  }, {
+    id: '3',
+    text: 'comment number 3'
+  }
+];
+
+export { Comments };
