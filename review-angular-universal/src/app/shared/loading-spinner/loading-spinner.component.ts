209657da8b25069ebd90d9baf215c8d1cf6d00c9
ohisee <breakosin@hotmail.com>
2023-08-22 23:15:33 -0700

    Create loading spinner

---
+++ review-angular-universal/src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'loading-spinner',
+  templateUrl: './loading-spinner.component.html',
+  styleUrls: ['./loading-spinner.component.css']
+})
+export class LoadingSpinnerComponent {
+}
