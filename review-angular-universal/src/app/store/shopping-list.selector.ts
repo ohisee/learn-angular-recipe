8ad0587c84771ad003c3644b2424e4136c70466c
ohisee <breakosin@hotmail.com>
2023-09-16 00:01:47 -0700

    Create shopping list selectors

---
+++ review-angular-universal/src/app/store/shopping-list.selector.ts
@@ -0,0 +1,11 @@
+import { createSelector } from "@ngrx/store";
+
+import { RecipeAppState } from "./index";
+import { ShoppingListState } from "./shopping-list.reducer"
+
+const selectShoppingList = (state: RecipeAppState) => state.shoppingList;
+
+export const selectIngredientList = createSelector(
+  selectShoppingList,
+  (state: ShoppingListState) => state.ingredients
+);
