1a7b0ddf8c8226fb6255ceda69fdb2c9586cdc32
ohisee <breakosin@hotmail.com>
2024-04-01 01:41:36 -0700

    create a node js app

--- node-html-ejs/tools/naturalLangDate.js
+++ node-html-ejs/tools/naturalLangDate.js
@@ -9,3 +9,5 @@ const date = chrono.parseDate('Meeting at 1:30 PM');
 // Intl.DateTimeFormat('en-US').resolvedOptions().timeZone
 console.log(date.toLocaleString('en-US', { timeZoneName: 'long' }));
 
+// 5/1/2024, 1:30:00 PM Pacific Daylight Time
+
