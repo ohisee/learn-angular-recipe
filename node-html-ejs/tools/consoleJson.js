57d09766eac0da3e7f441b003fcf550922d09d1a
ohisee <breakosin@hotmail.com>
2024-04-19 00:07:38 -0700

    create a node js app

---
+++ node-html-ejs/tools/consoleJson.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview console
+ */
+const readline = require('node:readline');
+const process = require('node:process');
+const EventEmitter = require('node:events');
+const emitter = new EventEmitter();
+
+
