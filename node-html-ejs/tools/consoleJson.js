192d662eac33cf670fdfb72abc7b55730d2d7f24
ohisee <breakosin@hotmail.com>
2024-04-20 00:18:01 -0700

    create a node js app

--- node-html-ejs/tools/consoleJson.js
+++ node-html-ejs/tools/consoleJson.js
@@ -6,4 +6,9 @@ const process = require('node:process');
 const EventEmitter = require('node:events');
 const emitter = new EventEmitter();
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 
