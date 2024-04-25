587d6bf2ad85cd620af529924aeeba54f81e1cee
ohisee <breakosin@hotmail.com>
2024-04-25 00:07:51 -0700

    create a node js app

--- node-html-ejs/tools/check.js
+++ node-html-ejs/tools/check.js
@@ -2,8 +2,11 @@
  * @fileoverview how to check this JavaScript code 
  */
 const Walker = require('./walker');
+const {fibo, printLine} = require('./reviewExport');
 
 const check = new Walker('Speed Train', 1000.91);
 
 check.sayYourName();
+
+printLine('hello')(fibo(9));
 
