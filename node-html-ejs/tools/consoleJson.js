43fe8989f46dc7e84d90e9e98d3f8ec74f2b6058
ohisee <breakosin@hotmail.com>
2024-05-01 00:02:52 -0700

    create a node js app

--- node-html-ejs/tools/consoleJson.js
+++ node-html-ejs/tools/consoleJson.js
@@ -11,4 +11,18 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+function showJson(/** @type String */ input) {
+  setTimeout(() => {
+    if (input === '') {
+      console.log("empty input");
+      return;
+    }
+    const start = 0 + 1;
+    const end = input.lastIndexOf('"');
+    const inputStr = input.substring(start, end);
+    const jj = JSON.parse(inputStr.replace(/\\/g, ''));
+    const fj = JSON.stringify(jj, null, 2);
+    console.log('\x1b[32m%s\x1b[0m', fj);
+  }, 100);
+}
 
