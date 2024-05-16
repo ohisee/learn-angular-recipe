c5e2c6fd92ba7ee69306e422420e470c054899c4
ohisee <breakosin@hotmail.com>
2024-05-16 00:07:30 -0700

    create a node js app

---
+++ node-html-ejs/tools/dimension.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview dimension
+ */
+const dimension = {
+  key: 'runner',
+  title: 'San Jose',
+  name: 'San Mateo',
+  isUp: true,
+  hidden: {
+    something: ['car', 'truck']
+  }
+};
+
+module.exports = {
+  dimension
+};
+
