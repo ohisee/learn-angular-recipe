4d3cc81e462722c7b309ffaf53987861097c8454
ohisee <breakosin@hotmail.com>
2024-05-20 00:07:14 -0700

    create a node js app

--- node-html-ejs/tools/dimension.js
+++ node-html-ejs/tools/dimension.js
@@ -1,17 +1,40 @@
 /**
  * @fileoverview dimension
  */
+
+/**
+ * @typedef {{key: string, title: string, name: string, isUp: boolean, isReady: boolean, hidden: []}} Dimension 
+ */
+
+/** @type {Dimension} */
 const dimension = {
   key: 'runner',
   title: 'San Jose',
   name: 'San Mateo',
   isUp: true,
+  isReady: false,
   hidden: {
     something: ['car', 'truck']
   }
 };
 
+/**
+ * @returns {Promise<Dimension>}
+ */
+function updateDimension() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        ...dimension,
+        isReady: true
+      });
+    }, 2000);
+  });
+}
+
 module.exports = {
-  dimension
+  dimension,
+  updateDimension
 };
+
 
