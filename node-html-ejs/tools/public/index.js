859325ce21fbc4c69e32fded78a424acfdfbcf7d
ohisee <breakosin@hotmail.com>
2024-05-28 00:02:01 -0700

    create a node js app

--- node-html-ejs/tools/public/index.js
+++ node-html-ejs/tools/public/index.js
@@ -2,13 +2,25 @@
 (function() {
   /** @type {HTMLDivElement} */
   const main = document.querySelector(".main_header");
+  /** @type {HTMLDivElement} */
+  const backdrop = document.querySelector('.backdrop');
+  /** @type {HTMLButtonElement} */
+  const buttonBackdrop = document.querySelector('.b_backdrop');
 
-  main.addEventListener("click", function () {
+  main.addEventListener('click', function () {
     if (main.style.background === '') {
       main.style.background = "#d2c9c9";
     } else {
       main.style.background = '';
     }
+  });
+
+  buttonBackdrop.addEventListener('click', function () {
+    backdrop.style.display = 'block';
+  });
+
+  backdrop.addEventListener('click', function () {
+    backdrop.style.display = 'none';
   });
 
 })();
