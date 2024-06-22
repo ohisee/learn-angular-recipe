fbb13a5630d066c197e17ac9c457dfe3fb470f4a
ohisee <breakosin@hotmail.com>
2024-06-22 00:34:52 -0700

    create a node js app

--- node-html-ejs/tools/public/queryResult.js
+++ node-html-ejs/tools/public/queryResult.js
@@ -8,6 +8,8 @@
   const searchResultDivEl = document.querySelector('.search__result');
   /** @type {HTMLUListElement} */
   const searchResultUlEl = document.querySelector('.search__result ul');
+  /** @type {String} li clicked event */
+  const resultItemClickedEvent = 'resultItemClicked';
 
   inputEL.addEventListener('input', function () {
     searchResultDivEl.classList.add('with_border');
@@ -16,14 +18,22 @@
     // liEl.innerText = 'new element';
     // searchResultUlEl.insertAdjacentElement('beforeend', liEl);
 
+
     if (this.value) {
       fetch('/service/api/simple/query/options').then(res => res.json()).then(
         res => {
           const options = res['result'];
           if (options) {
-            for (let r of options) {
+            for (let resultOption of options) {
               const liEl = document.createElement('li');
-              liEl.innerText = r;
+              liEl.innerText = resultOption;
+              liEl.onclick = function (event) {
+                const customEvent = new CustomEvent(resultItemClickedEvent, {
+                  bubbles: true,
+                  detail: { text: resultOption }
+                });
+                event.target.dispatchEvent(customEvent);
+              };
               searchResultUlEl.insertAdjacentElement('beforeend', liEl);
             }
           }
