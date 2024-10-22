592bca2404ce074cc4c043f21dc70cf280d94a37
ohisee <breakosin@hotmail.com>
2024-10-22 00:15:07 -0700

    create a node js app

--- node-html-ejs/tools/public/shadowDom.js
+++ node-html-ejs/tools/public/shadowDom.js
@@ -7,6 +7,15 @@ class ShowYourName extends HTMLElement {
     super();
   }
 
+  connectedCallback() {
+    console.log(`Show your name component ${this.isConnected}`);
+
+    const shadow = this.attachShadow({mode: 'open'});
+    shadow.innerHTML = `<p>Hello, ${this.getAttribute('name')} </p>`;
+    const sheet = this._createStyleSheet();
+    shadow.adoptedStyleSheets = [sheet];
+  }
+
   _createStyleSheet() {
     const sheet = new CSSStyleSheet();
     sheet.replaceSync(`p { font-family: sans-serif; margin: 16px; }`);
