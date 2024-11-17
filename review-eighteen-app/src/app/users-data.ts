59ff91849c7320a81774782b95e4a70a72dfb88b
ohisee <breakosin@hotmail.com>
2024-11-16 20:42:50 -0800

    create one anguler 18 app

---
+++ review-eighteen-app/src/app/users-data.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Jasmine Washington',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Johnnie Walker',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Marcus Johnson',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'David Miller',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'James Walker',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Jerry Walker',
+    avatar: 'user-6.jpg',
+  },
+];
