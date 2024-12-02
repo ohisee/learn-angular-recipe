7b7675c93b7d1af4450216890c2d406733c05cfa
ohisee <breakosin@hotmail.com>
2024-12-02 00:06:49 -0800

    create one anguler 18 app

--- review-eighteen-app/src/app/app.component.ts
+++ review-eighteen-app/src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './components/user/user.component';
+import { DUMMY_USERS } from './users-data';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent { }
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId: string = '';
+
+  /**
+   * Getter
+   */
+  public get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId);
+  }
+
+  /**
+   * Select user handler
+   */
+  public onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
+}
