e20b1ab4656c0f1593e221f7034c643f46c910ed
ohisee <breakosin@hotmail.com>
2024-12-28 00:31:56 -0800

    create one anguler 18 app

---
+++ review-eighteen-app/src/app/components/tasks/task/task.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { type Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
+import { TasksService } from '../tasks.service';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [CardComponent, DatePipe],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+
+  // @Output() complete = new EventEmitter<string>();
+
+  private readonly tasksService = inject(TasksService);
+
+  public onCompleteTask() {
+    // this.complete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id);
+  }
+}
