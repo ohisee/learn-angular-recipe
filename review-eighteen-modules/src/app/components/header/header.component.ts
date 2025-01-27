a763ba27d2ffd58763eae2e2dcdc2e4645e4bac5
ohisee <breakosin@hotmail.com>
2025-01-27 00:38:45 -0800

    create one anguler 18 app

---
+++ review-eighteen-modules/src/app/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-header',
+  standalone: false,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent { }
