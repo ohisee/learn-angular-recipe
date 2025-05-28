08f986876798af13e0baa813d24c888888ece942
ohisee <breakosin@hotmail.com>
2025-05-28 00:02:49 -0700

    create one anguler 19 app

---
+++ review-nineteen-pipe/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { TemperaturePipe } from './temperature.pipe';
+import { SortPipe } from './sort.pipe';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [DatePipe, DecimalPipe, TemperaturePipe, SortPipe]
+})
+export class AppComponent {
+  currentDate = new Date();
+  currentTemperaturs = {
+    berlin: 4.2749812,
+    newYork: 18.1214,
+    paris: 72.1209001,
+    chicago: 65.0775238,
+    sanJose: 100.112863
+  };
+
+  historicTemperatures = [
+    25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
+  ];
+
+  constructor() {
+    this.historicTemperatures.sort((a, b) => a > b ? 1 : -1);
+  }
+
+  onReset(index: number) {
+    this.historicTemperatures[index] = 18;
+    // create a new copy of array to trigger Pipe to change 
+    // const temp = [...this.historicTemperatures];
+    // temp[index] = 18;
+    // this.historicTemperatures = temp;
+  }
+}
