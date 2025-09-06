e33a8521a8b9574a9783032d7e5d65107bfc0bd8
wei <breakosin@hotmail.com>
2025-09-06 02:18:18 -0700

    Create a Angular 19 cmp app

---
+++ review-nineteen-cmp-deep-dive/src/app/components/dashboard/server-status/server-status.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ServerStatusComponent } from './server-status.component';
+
+describe('ServerStatusComponent', () => {
+  let component: ServerStatusComponent;
+  let fixture: ComponentFixture<ServerStatusComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ServerStatusComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ServerStatusComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
