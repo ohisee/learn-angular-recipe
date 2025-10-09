9b69176d1a136dfed7f55a426de1e6cc7ae95625
wei <breakosin@hotmail.com>
2025-10-09 01:45:32 -0700

    Create a Angular 19 service app

---
+++ review-nineteen-services/src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
+// import { TasksService } from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  templateUrl: './tasks.component.html',
+  imports: [NewTaskComponent, TasksListComponent],
+  // providers: [TasksService] // tied to element injector and available to child components 
+  // each instance of TasksComponent will get a different separated independent instance of TasksService
+})
+export class TasksComponent { }
