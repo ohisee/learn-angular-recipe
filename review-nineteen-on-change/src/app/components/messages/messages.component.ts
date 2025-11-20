2b694c03cda2870ffdf41ad37eaeb06386293005
wei <breakosin@hotmail.com>
2025-11-20 01:34:04 -0800

    Create a Angular 19 on change app

---
+++ review-nineteen-on-change/src/app/components/messages/messages.component.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+
+import { MessagesListComponent } from './messages-list/messages-list.component';
+import { NewMessageComponent } from './new-message/new-message.component';
+
+@Component({
+  selector: 'app-messages',
+  standalone: true,
+  templateUrl: './messages.component.html',
+  styleUrl: './messages.component.css',
+  imports: [MessagesListComponent, NewMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush, // less change detection 
+  // when this is a change in this component or in any of its child components, trigger change detection
+})
+export class MessagesComponent {
+  // messages = signal<string[]>([]);
+
+  get debugOutput() {
+    console.log('[Messages] "debugOutput" binding re-evaluated.');
+    return 'Messages Component Debug Output';
+  }
+
+  // onAddMessage(message: string) {
+  //   this.messages.update((oldMessages) => [...oldMessages, message]);
+  // }
+}
